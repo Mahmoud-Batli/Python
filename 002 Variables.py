864cd8dbcce4522aa6ecf8a81306f81f1c210786
Mahmoud Batli <mahmoudbatli@gmail.com>
2026-01-05 20:18:19 +0200

    Update 002 Variables.py

--- 002 Variables.py
+++ 002 Variables.py
@@ -1,2 +1,22 @@
-name = ("Mahmoud")
+name = "Mahmoud Batli" # Normal
+myName = "Mahmoud Batli" # camel Case
+my_name = "Mahmoud Batli" # snake Case
+MyName = "Mahmoud Batli" # Pascal Case
+
 print(name)
+print(myName)
+print(my_name)
+print(MyName)
+
+x = 5
+x = "name"
+print(x)
+
+help("keywords") # Shows Reserved Words in Python
+
+a, b, c = 5, 10, 15
+print(a)
+print(b)
+print(c)
+
+
