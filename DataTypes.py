228d22d3f44dce4f9029b403206c3599c341a295
Mahmoud Batli <mahmoudbatli@gmail.com>
2026-01-05 19:35:00 +0200

    Create DataTypes.py

---
+++ DataTypes.py
@@ -0,0 +1,11 @@
+print("Hello Python")
+print("Ilove Programming")
+# Comment
+print(type(100)) # Integer
+print(type(10.5)) # Float
+print(type(2 == 2)) # Boolean
+print(type([1, 2, 3, 4])) # List 
+print(type((1, 2, 3, 4))) # Tuple
+print(type({1, 2, 3, 4})) # Set
+print(type({"name": "Alice", "age": 25})) # Dictionary
+
