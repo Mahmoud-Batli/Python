753c7a5bf46ef22aee155cf3bb2fff35a71c8f1d
Mahmoud Batli <mahmoudbatli@gmail.com>
2026-01-05 20:55:02 +0200

    Create 003 Escape Sequances Characters.py

---
+++ 003 Escape Sequances Characters.py
@@ -0,0 +1,32 @@
+# Back Space \b
+print("Hello\bWorld")  # Remove the character before \b o
+
+# Escape new line + backslash \
+print("Hello \
+World")  # New line after Hello
+
+# Escape Backslash \\
+print("Hello\\World")  # Print backslash between Hello and World
+
+# Escape Single Quote \'
+print('It\'s a beautiful day')  # Print single quote in the string
+
+# Escape Double Quote \"
+print("He said, \"Hello World\"")  # Print double quotes in the string
+
+# line Feed \n
+print("Hello\nWorld")  # New line between Hello and World
+
+# Carriage Return \r
+print("123456\rabcd")  # Overwrite 1234 with abcd
+
+# Horizontal Tab \t
+print("Hello\tWorld")  # Tab space between Hello and World
+
+# Vertical Tab \v
+print("Hello\vWorld")  # Vertical tab between Hello and World
+
+# character hexadecimal value \xhh
+print("Hello\x20World")  # Space between Hello and World using hex value * search Goole for more hex values
+
+
