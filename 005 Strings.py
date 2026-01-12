1d2739b7752cee115c290d544c5b636236aa9e11
Mahmoud Batli <mahmoudbatli@gmail.com>
2026-01-12 13:09:07 +0200

    Strings

---
+++ 005 Strings.py
@@ -0,0 +1,47 @@
+myStringone = 'This is Single Quotes'
+myStringtwo = "This is Double Quotes"
+print(myStringone)
+print(myStringtwo)
+
+myStringthree = '''This is Triple Quotes'''
+myStringfour = """This is also Triple Quotes"""
+print(myStringthree)
+print(myStringfour)
+
+multiLineString = '''This is a multi-line string.
+It can span multiple lines.
+You can write as much as you want here.'''
+print(multiLineString)
+
+myString = "I Love Python"
+print(myString[0])      # First character
+print(myString[7])      # Eighth character
+print(myString[-1])     # Last character
+print(myString[-7])     # Seventh from last character
+print(myString[2:6])    # Characters from index 2 to 5
+print(myString[:6])     # First five characters 
+print(myString[7:])     # Characters from index 7 to end
+print(myString[-6:])    # Last six characters
+print(myString[:-7])    # All but last six characters
+print(myString[:])      # The whole string
+print(myString[::2])    # Every second character
+print(myString[1::2])   # Every second character starting from index 1
+print(myString[::-1])   # The whole string in reverse
+
+# String Methods
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
